f54c3066f845d4b6994620304eaf301c3e542966
Praveen Sharma <praveen.kr.sharma02@gmail.com>
2025-02-06 19:30:52 +0530

    added swagger support

--- src/main/java/com/poc/elastic_db_ms_it/controller/ProductController.java
+++ src/main/java/com/poc/elastic_db_ms_it/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.poc.elastic_db_ms_it.controller;
 
 import com.poc.elastic_db_ms_it.model.Product;
 import com.poc.elastic_db_ms_it.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
+@Tag(name = "Product API", description = "CRUD operations for products")
 public class ProductController {
 
     private final ProductService productService;
@@ -25,21 +28,25 @@ public class ProductController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a new product", description = "Adds a new product to the database")
     public Product createProduct(@RequestBody Product product) {
        return productService.saveProduct(product);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get product by ID", description = "Retrieves a product by its unique ID")
     public Optional<Product> getProductById(@PathVariable String id) {
         return productService.getProductById(id);
     }
 
     @GetMapping
+    @Operation(summary = "Get all products", description = "Retrieves a list of all available products")
     public Iterable<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a product", description = "Deletes a product by its ID")
     public void deleteProduct(@PathVariable String id) {
         productService.deleteProduct(id);
     }
